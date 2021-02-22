cfbb4e955d6d24cfaf1b46626053dc5a764a8ae0
ismoi <ismoilov.munir97@gmail.com>
2021-02-23 01:33:38 +0300

    feat .env example

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '0999fb6e1d76a9581bf1ad4fb790d084'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '01a961e0-3c43-4bd4-bfe6-f3413682267d'
+};
